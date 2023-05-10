7682c87f2cc8d93c154d0e55da28b5c4850a84c7
surya-sanity <surya.sanity@gmail.com>
2023-05-10 17:27:35 +0530

    bookmarks test route changed.

--- src/Screens/Bookmarks/Bookmarks.tsx
+++ src/Screens/Bookmarks/Bookmarks.tsx
@@ -1,7 +1,9 @@
 import HeaderTabItem from '../../Components/PageHeader/HeaderTabItem'
 import PageHeader from '../../Components/PageHeader/PageHeader'
+import { useDefaultTabNav } from '../../Hooks/useDefaultTabNav'
 
 const Bookmarks = () => {
+  useDefaultTabNav({ endsWith: 'bookmarks', navTo: "/bookmarks/one" })
   return (
     <PageHeader
       name="Bookmarks">
